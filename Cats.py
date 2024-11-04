119f37cd4fcc6aa1655ca6da65d3a5faf5a2396d
Dasha51 <k67302913@gmail.com>
2024-11-04 15:31:35 +0300

    создана ф-ция для получения изображения с котиком

--- Cats.py
+++ Cats.py
@@ -3,6 +3,19 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+
+def load_image(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print (f"Произошла ошибка")
+        return None
+
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
@@ -10,7 +23,7 @@ window.geometry("600x480")
 label = Label()
 label.pack()
 
-URL = "https://cataas.com/cat"
+url = "https://cataas.com/cat"
 img = load_image(url)
 
 if img:
